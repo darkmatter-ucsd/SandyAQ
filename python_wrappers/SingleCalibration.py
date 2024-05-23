10bf9373ccef9cf24944b91dc7d70205b57a6a6e
yuema137 <3124558229@qq.com>
2024-05-22 17:18:51 -0700

    Add threshold calibration before every run

--- python_wrappers/SingleCalibration.py
+++ python_wrappers/SingleCalibration.py
@@ -11,19 +11,39 @@ import datetime
 import json
 from copy import deepcopy
 from tqdm import tqdm
+import glob
+import numpy as np
+import sys
+import configparser
+import json
+
+import WaveformProcessor
+import util
+sys.path.insert(0,"/home/daqtest/Processor/sandpro")
+import sandpro
 
 class SingleCalibration:
-    def __init__(self, data_taking_settings, config_template_path = "/home/daqtest/DAQ/SandyAQ/sandyaq/config/config_template.ini"):
+    def __init__(self, data_taking_settings, 
+                 config_template_path = "/home/daqtest/DAQ/SandyAQ/sandyaq/config/config_template.ini", 
+                 calibration = True):
+        
         # check if the data_taking_settings is a dictionary, and have all of the required keys
         if not isinstance(data_taking_settings, dict):
             raise ValueError("data_taking_settings should be a dictionary")
-        if not all(key in data_taking_settings for key in ["channel_threshold_dict",
-                                                           "number_of_events",
+        # if not all(key in data_taking_settings for key in ["channel_threshold_dict",
+        #                                                    "number_of_events",
+        #                                                    "output_folder", 
+        #                                                    "voltage_config", 
+        #                                                    "temperature"]):
+        # raise ValueError("data_taking_settings should have all of the required keys: \
+        #                      channel_threshold_dict, number_of_events, output_folder, voltage_config, temperature")
+        
+        if not all(key in data_taking_settings for key in ["number_of_events",
                                                            "output_folder", 
                                                            "voltage_config", 
                                                            "temperature"]):
             raise ValueError("data_taking_settings should have all of the required keys: \
-                             channel_threshold_dict, number_of_events, output_folder, voltage_config, temperature")
+                              number_of_events, output_folder, voltage_config, temperature")
         self.data_taking_settings = data_taking_settings
 
         # check if the config_template is a string and exists
@@ -36,20 +56,38 @@ class SingleCalibration:
         self.config_template = configparser.ConfigParser()
         self.config_template.optionxform = str
         self.config_template.read(config_template_path)
-        self.tmp_config_files = self.generate_temp_configs()
 
-    def generate_temp_configs(self):
+        if calibration:
+            print("Running calibration...")
+            self.run_calibration()
+        else:
+            # generate the temporary config files
+            self.tmp_config_files = self.generate_temp_configs()
+
+    def generate_temp_configs(self, calibration = False):
         # the template config is an ini file. We need to do the following modifications:
         # 1. add the channel list
         # 2. add the thresholds
 
+        if calibration:
+            # set a very low threshold for all channels, to take noise data
+            data_settings = self.data_taking_settings.copy()
+            data_settings["channel_threshold_dict"] = {0:2305,1:2269,2:2395,3:2209,4:2378,5:2274,6:2386,7:2516,8:2627,9:2316,10:2162,11:2166,12:2456,13:2456,14:2413,15:2192,16:2230,17:2468,18:2158,19:2090,20:2342,21:2258,22:2313,23:2266}
+            data_settings["number_of_events"] = 1500
+            data_settings["output_folder"] = data_settings["output_folder"] + "/threshold_calibration"
+        else:
+            data_settings = self.data_taking_settings
+        
+        temp_folder = os.path.join(data_settings["output_folder"], "tmp")
+
+        print("Thrsesholds: ", data_settings["channel_threshold_dict"])
+
         # first: make a tmp folder to store the temp config files
-        temp_folder = os.path.join(self.data_taking_settings["output_folder"], "tmp")
         if not os.path.exists(temp_folder):
             os.makedirs(temp_folder)
         tmp_config_files = []
-        for channel in self.data_taking_settings["channel_threshold_dict"].keys():
-            new_config_path = os.path.join(temp_folder, f"config_{channel}_{self.data_taking_settings['channel_threshold_dict'][channel]}.ini")
+        for channel in data_settings["channel_threshold_dict"].keys():
+            new_config_path = os.path.join(temp_folder, f"config_{channel}_{data_settings['channel_threshold_dict'][channel]}.ini")
 
             # skip if the config file already exists
             if os.path.exists(new_config_path):
@@ -74,9 +112,10 @@ class SingleCalibration:
 
             # Add the new configuration to the BOARD-0 section
             new_config.set(section_name, 'DC_OFFSET', '+40')
-            new_config.set(section_name, 'TRIGGER_THRESHOLD', f'{self.data_taking_settings["channel_threshold_dict"][channel]}')
+            new_config.set(section_name, 'TRIGGER_THRESHOLD', f'{data_settings["channel_threshold_dict"][channel]}')
             new_config.set(section_name, 'CHANNEL_TRIGGER', 'ACQUISITION_ONLY')
             new_config.set(section_name, 'PULSE_POLARITY', '1')
+            new_config.set(section_name, 'N_EVENTS', f'{data_settings["number_of_events"]}')
 
             # write the new config to a file. The file name is the channel number and the threshold
             with open(new_config_path, "w") as f:
@@ -84,7 +123,25 @@ class SingleCalibration:
             tmp_config_files.append(new_config_path)
         return tmp_config_files
     
-    def run(self, dry_run = False):
+    def run_calibration(self, dry_run = False):
+        # settings for the calibration run
+        self.tmp_config_files = self.generate_temp_configs(calibration=True)
+        self.run(dry_run=False, calibration=True)
+        
+        # clear output (doesn't work in jupyter notebook)
+        # os.system('clear')
+
+        # get threshold
+        print("Determining thresholds from calibration run...")
+        self.data_taking_settings["channel_threshold_dict"] = self.get_adc_threshold_from_calibration()
+
+        self.tmp_config_files = self.generate_temp_configs(calibration=False)
+
+    def run(self, dry_run = False, calibration = False):
+        
+        # get the current timestamp, and add it to the file name
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
         for tmp_config_file in tqdm(self.tmp_config_files):
             # -c: config file
             # -n: number of events
@@ -94,9 +151,19 @@ class SingleCalibration:
             # extract the file name from the tmp config file
             data_file_name = os.path.basename(tmp_config_file).replace(".ini", "")
             # get the current timestamp, and add it to the file name
-            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            # moved this out of the loop: so that all channel from the same run has the same timestamp
+            # timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
             data_file_name = f"{data_file_name}_{timestamp}"
-            meta_file_name = os.path.join(self.data_taking_settings["output_folder"], f"meta_{data_file_name}.json")
+
+            if calibration:
+                base_path = os.path.join(self.data_taking_settings["output_folder"], "threshold_calibration")
+                # meta_file_name = os.path.join(base_path, f"meta_{data_file_name}.json")
+            else:
+                base_path = self.data_taking_settings["output_folder"]
+                # meta_file_name = os.path.join(self.data_taking_settings["output_folder"], f"meta_{data_file_name}.json")
+                
+
+            meta_file_name = os.path.join(base_path, f"meta_{data_file_name}.json")
 
             with open(meta_file_name, "w") as f:
                 json.dump(self.data_taking_settings, f)
@@ -110,12 +177,110 @@ class SingleCalibration:
             if not dry_run:
                 try:
                     print(f"Taking data with config file: {tmp_config_file}")
-                    os.system(f"{run_sandyaq_command} -c {tmp_config_file} -n {self.data_taking_settings['number_of_events']} -d {self.data_taking_settings['output_folder']} -f {data_file_name}")
+                    os.system(f"{run_sandyaq_command} -c {tmp_config_file} -n {self.data_taking_settings['number_of_events']} -d {base_path} -f {data_file_name}")
                 except Exception as e:
                     print(f"Error running sandyaq: {e}")
             else:
-                print(f"Running sandyaq -c {tmp_config_file} -n {self.data_taking_settings['number_of_events']} -d {self.data_taking_settings['output_folder']} -f {data_file_name}")
+                print(f"Running sandyaq -c {tmp_config_file} -n {self.data_taking_settings['number_of_events']} -d {base_path} -f {data_file_name}")
+ 
+    def get_adc_threshold_from_calibration(self):
+        # read the data from the calibration run
+        data_folder = os.path.join(self.data_taking_settings["output_folder"],"threshold_calibration")
+        meta_data_list = glob.glob(f"{data_folder}/*meta*")
+
+        # Sort the file paths based on the extracted date and time in ascending order (oldest to newest)
+        new_meta_data_list = sorted(meta_data_list, key=util.extract_date_meta_data)
+        meta_data_list=new_meta_data_list[-24:]
+
+        # Check if all calibration runs are successful
+        # by checking if the timestamps are the same
+        datetime_list = util.v_extract_date_meta_data(meta_data_list)
+        if not np.all(datetime_list == datetime_list[0]):
+            raise ValueError("Not all calibration runs are successful: \nthe last 24 calibration runs don't have the same timestamps. \nCheck the data. Exiting.")
+        
+        # Sort the meta data according to channel number
+        sorted_meta_data_list = sorted(meta_data_list, key=util.extract_channel_meta_data)
+
+        # Get the thresholds from the last 24 calibration runs
+        baseline_mean_array = []
+        baseline_std_array = []
+
+        for i, meta_data in enumerate(sorted_meta_data_list):
+            print(sorted_meta_data_list[i])
+            # meta_data = sorted_meta_data_list[i]
+            meta_data_basename = os.path.basename(meta_data)
+            parts = meta_data_basename.split('_')
+            config_name = "_".join(parts[1:4]) + ".ini"
+
+            print(config_name)
+            config_path = os.path.join(data_folder, "tmp", config_name)
+            config = configparser.ConfigParser()
+            config.optionxform = str
+            config.read(config_path)
+
+            process_config = {"nchs": 1,
+                "nsamps": int(config.get("COMMON", "RECORD_LENGTH")),
+                "sample_selection": 120,
+                "samples_to_average": 40
+                }
             
+            # dump the config to a json file
+            with open("process_config.json", "w") as f:
+                json.dump(process_config, f)
+            if len(config.get("BOARD-0", "CHANNEL_LIST")) > 0:
+                board_number = 0
+            else:
+                board_number = 1
+
+            processor= sandpro.processing.rawdata.RawData(config_file = "process_config.json",
+                                                perchannel=False)
+            data_file_basename = meta_data_basename.replace("meta_", "").replace(".json", f"_board_{board_number}.bin")
+            n_evts = self.data_taking_settings["number_of_events"]
+            try:
+                data = processor.get_rawdata_numpy(n_evts=n_evts-1,
+                                            file=os.path.join(data_folder, data_file_basename),
+                                            bit_of_daq=14,
+                                            headersize=4,inversion=False)
+                start_index, end_index = 500, n_evts-1-500
+            except Exception as e:
+                print(e)
+                data = processor.get_rawdata_numpy(n_evts=200,
+                                            file=os.path.join(data_folder, data_file_basename),
+                                            bit_of_daq=14,
+                                            headersize=4,inversion=False)
+                start_index, end_index = 0, 119
+            
+            wfp = WaveformProcessor.WFProcessor(data_folder, volt_per_adc=2/2**14)
+            wfp.set_data(data["data_per_channel"][start_index:end_index,0], in_adc = False)
+            wfp.process_wfs()
+            
+            baseline_std = np.mean(wfp.baseline_rms)
+            baseline_mean = np.mean(wfp.baseline)
+            baseline_mean_array.append(baseline_mean)
+            baseline_std_array.append(baseline_std)
+
+        baseline_mean_array = np.array(baseline_mean_array)
+        baseline_std_array = np.array(baseline_std_array)
+        # threshold_mV = (baseline_mean_array + 2 * (2 * np.sqrt(2) * baseline_std_array)) * 1000
+        threshold_mV = (baseline_mean_array + 3 * baseline_std_array) * 1000
+        threshold_adc = util.v_mv_to_adc(threshold_mV)
+        
+        threshold_dict = {i:int(thres) for i, thres in enumerate(threshold_adc.astype(int))}
+
+        return threshold_dict
+    
+    
+# for debugging
+# if __name__ == "__main__":
+#     test_config = {}
+#     test_config["channel_threshold_dict"] = {0:2000,1:2319,2:2618,3:2273,4:2426,5:2341,6:2468,7:2567,8:2801,9:2371,10:2210,11:2352,12:2470,13:2496,14:2602,15:2264,16:2284,17:2531,18:2216,19:2147,20:2522,21:2306,22:2369,23:2465}
+#     test_config["number_of_events"] = 5000
+#     test_config["output_folder"] = "./test_calibration"
+#     test_config["voltage_config"] = {"preamp_1": -49, "preamp_2": -49,"preamp_3": -49,"preamp_4": -49,"preamp_5": -49}
+#     test_config["temperature"] = -98
+
+#     test_calibration = SingleCalibration(test_config, calibration=True)
+
         
         
 
---
+++ python_wrappers/util.py
@@ -0,0 +1,59 @@
+"""
+This script contains common utility functions that are used in 
+multiple scripts.
+"""
+
+from datetime import datetime
+import numpy as np
+import os
+
+def extract_date_meta_data(meta_data_path):
+
+    #reorganize the order of the meta_data_list, so the final one should be the newest one.
+    parts = meta_data_path.split('_')
+    date_part = parts[-2]
+    time_part = parts[-1].split('.')[0]  # Remove the file extension
+    datetime_str = date_part + '_' + time_part
+
+    # Parse the datetime using the format YYYYMMDD_HHMMSS
+    return datetime.strptime(datetime_str, '%Y%m%d_%H%M%S')
+
+v_extract_date_meta_data = np.vectorize(extract_date_meta_data)
+
+def extract_channel_meta_data(meta_data_path):
+
+    meta_data_basename = os.path.basename(meta_data_path)
+    parts = meta_data_basename.split('_')
+    # config_name = "_".join(parts[1:4]) + ".ini"
+    channel = int(parts[2])
+
+    return channel
+
+v_extract_channel_meta_data = np.vectorize(extract_channel_meta_data)
+
+def adc_to_mv(adc, DCOFFSET=+50, vpp=2.0, bit_of_daq=14):
+    #DC OFFSET = +40: -0.2 - 1.8
+    #DC OFFSET = +50: 0.0 - 2.0
+    #DC OFFSET = -50: -2 - 0.0
+    start_voltage = (DCOFFSET/50) -1
+    # end_voltage = start_voltage + vpp
+    # voltage_per_adc = vpp / (2**bit_of_daq-1)
+    return (adc/(2**bit_of_daq-1) * vpp + start_voltage) * 1000
+
+v_adc_to_mv = np.vectorize(adc_to_mv)
+
+def mv_to_adc(mv, DCOFFSET=+50, vpp=2.0, bit_of_daq=14):
+    start_voltage = (DCOFFSET/50) -1.0
+    # end_voltage = start_voltage + vpp
+    # voltage_per_adc = vpp / (2**bit_of_daq-1)
+    return (mv/1000 - start_voltage) / vpp * (2**bit_of_daq-1)
+
+v_mv_to_adc = np.vectorize(mv_to_adc)
+
+def V_to_adc(V, DCOFFSET=+40, vpp=2.0, bit_of_daq=14):
+    start_voltage = (DCOFFSET/50) -1.0
+    # end_voltage = start_voltage + vpp
+    # voltage_per_adc = vpp / (2**bit_of_daq-1)
+    return (V - start_voltage) / vpp * (2**bit_of_daq-1)
+
+v_V_to_adc = np.vectorize(mv_to_adc)
